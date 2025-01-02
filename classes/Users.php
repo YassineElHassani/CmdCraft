959e690aa1ad8c4df24f97ae52193504213b0456
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-02 16:42:44 +0100

    Users Class

---
+++ classes/Users.php
@@ -0,0 +1,60 @@
+<?php 
+require_once '../configs/Database.php';
+
+class Users {
+    protected $id;
+    protected $name;
+    protected $email;
+    protected $password;
+    protected $role;
+
+    public function __construct($id, $name, $email, $password, $role) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+
+    function getRole() {
+        return $this->role;
+    }
+
+    function setName($name) {
+        $this->name = $name;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setPassword($password) {
+        $this->password = $password;
+    }
+
+    function setRole($role) {
+        $this->role = $role;
+    }
+
+    function login() {
+
+    }
+}
+
+?>
