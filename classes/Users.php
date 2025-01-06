505328482109e385d9a4b0a0e66ff21b3d2181b6
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-06 21:04:27 +0100

    Adding the login & register functions

--- classes/Users.php
+++ classes/Users.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once '../configs/Database.php';
 
 class Users {
@@ -7,13 +7,15 @@ class Users {
     protected $email;
     protected $password;
     protected $role;
+    protected $isActive;
 
-    public function __construct($id, $name, $email, $password, $role) {
+    public function __construct($id = null, $name = null, $email = null, $password = null, $role = null, $isActive = null) {
         $this->id = $id;
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
         $this->role = $role;
+        $this->isActive = $isActive;
     }
 
     function getId() {
@@ -36,6 +38,10 @@ class Users {
         return $this->role;
     }
 
+    function getActivation() {
+        return $this->isActive;
+    }
+
     function setName($name) {
         $this->name = $name;
     }
@@ -52,9 +58,70 @@ class Users {
         $this->role = $role;
     }
 
-    function login() {
+    function setActivation($isActive) {
+        $this->isActive = $isActive;
+    }
 
+    public function register($name, $email, $password) {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        $user = $stmt->fetch();
+
+        if ($user) {
+            session_start();
+            
+            $_SESSION['already'] = true;
+            
+            header('Location: ./index.php');
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $stmt = $conn->prepare("INSERT INTO users (name, email, password, role, is_active) VALUES (?, ?, ?, 'customer', true)");
+        $stmt->execute([$name, $email, $hashedPassword]);
+    }
+
+    public function logIn($email, $password) {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        $user = $stmt->fetch();
+        
+        if($user && password_verify($password, $user['password'])) {
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['role'] = $user['role'];
+            
+            if($user['role'] == "admin") {
+                if($user['is_active'] == 0) {
+                    $_SESSION['noPerm'] = true;
+                } else {
+                    header("Location: ../dashboard/index.php");
+                }
+            } elseif($user['role'] == "customer") {
+                if($user['is_active'] == 0) {
+                    $_SESSION['noPerm'] = true;
+                } else {
+                    header("Location: ../index.php");
+                }
+            }
+        } else {
+            $_SESSION['false'] = true;
+        }
+    }
+
+    public function getAllUsers() {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT id, name, email, is_active FROM users");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function activation() {
+        if(isset($_GET['id'])) {
+            $conn = Database::getConnection();
+            $stmt = $conn->prepare("UPDATE users SET is_active = 0 WHERE id = id");
+        }
     }
 }
-
-?>
