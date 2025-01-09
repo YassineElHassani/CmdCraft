ec998464060f79ddbea796a2f63e070c8b10b23a
Yassine El hassani <yassine.ds.404@gmail.com>
2025-01-09 21:25:42 +0100

    Adding all statistic functions

--- classes/Admin.php
+++ classes/Admin.php
@@ -2,15 +2,34 @@
 require_once '../configs/Database.php';
 
 
-class Admin extends Users {
+class Admin {
 
-    function manageUsers() {
-
+    public function totalUsers() {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM users");
+        $stmt->execute();
+        return $stmt->fetchColumn();
     }
 
+    public function totalActiveCustomers() {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM users WHERE is_active = 1 AND role = 'customer';");
+        $stmt->execute();
+        return $stmt->fetchColumn();
+    }
 
-    function viewStatistics() {
+    public function totalProducts() {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM products;");
+        $stmt->execute();
+        return $stmt->fetchColumn();
+    }
 
+    public function totalOrders() {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM orders;");
+        $stmt->execute();
+        return $stmt->fetchColumn();
     }
 
 }
